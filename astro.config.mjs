89b162c63587bd4be3ec428715210edd77eea9f9
Oleksandr Liakhov <oleksandr.liakhov-ext@jci.com>
2026-04-25 16:06:28 +0700

    Upgrade astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind({ applyBaseStyles: false })]
 });
