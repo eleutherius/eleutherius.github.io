78018f38187d17016ae7c93f66a48a06d6636108
Oleksandr Liakhov <oleksandr.liakhov-ext@jci.com>
2026-04-29 21:11:42 +0700

    Added workflow for deploy site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://eleutherius.github.io',
   integrations: [mdx(), sitemap()]
 });
